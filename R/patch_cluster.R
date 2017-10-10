0fa8faaacfb15cc6ae0a554eed9bd7ad90b919e6
Bradley Buchsbaum <bbuchsbaum@Fantail.local>
2017-10-09 23:50:23 -0400

    patch_cluster improved

--- R/patch_cluster.R
+++ R/patch_cluster.R
@@ -15,29 +15,33 @@ patch_cluster <- function(bvec, mask, K=500, patch_radius=8, connectivity=27,
     valmat <- filter_mat(valmat, filter$lp, filter$hp)
   }
 
-  slight <- neuroim::Searchlight(mask, radius=radius)
-  clist <- foreach(x=slight) %do% {
-    x
-  }
-
+  slight <- neuroim::Searchlight(mask, radius=patch_radius, eager=TRUE)
+  clist <- lapply(slight, identity)
 
   nn <- 27
   hmap <- hashmap("-1","-1")
-  nabes <- Neighbour(grid, grid, nn)
+  nabes <- rflann::Neighbour(grid, grid, nn)
   lapply(1:length(clist), function(i) {
     print(i)
     s1 <- series(bvec, clist[[i]])
-    sapply(nabes$indices[i,2:nn], function(j) {
-      key <- paste0(sort(c(i,j)), collapse="-")
-      if (hmap$has_key(key)) {
-        #message("has ", key)
-        hmap[[key]]
+
+
+    jind <- nabes$indices[i,2:nn]
+    p1 <- pmin(i, jind)
+    p2 <- pmax(i, jind)
+    knames <- paste(p1, p2, sep="-")
+
+    rvs <- sapply(1:length(jind), function(j) {
+      if (hmap$has_key(knames[j])) {
+        hmap[[knames[[j]]]]
       } else {
-        rv <- MatrixCorrelation::RV2(s1, series(bvec, clist[[j]]))
-        hmap[[key]] <- rv
-        rv
+        MatrixCorrelation::RV2(s1, series(bvec, clist[[jind[j]]]))
       }
     })
+
+    hmap[[knames]] <- rvs
+
+
   })
 
 
